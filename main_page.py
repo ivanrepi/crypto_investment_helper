c7c84692db7afaef307285285dd0bf711bdc8b53
Ivan Repilado <ivanrepi@hotmail.com>
2022-04-29 15:16:33 +0200

    main_page updated

--- main_page.py
+++ main_page.py
@@ -1,10 +1,7 @@
-from operator import ne
-from turtle import title
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
